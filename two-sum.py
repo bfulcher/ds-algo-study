299ca5ff98dd7dc6ef98e70f2db3b7f8afe63b74
Brendan Fulcher <brendan.fulcher@gmail.com>
2022-02-09 12:52:07 -0600

    Delete two-sum.py

--- two-sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevMap = {}
-        for element, num in enumerate(nums):
-            diff = target - num
-            if diff in prevMap:
-                return [prevMap[diff], element]
-            else:
-                prevMap[num] = element
